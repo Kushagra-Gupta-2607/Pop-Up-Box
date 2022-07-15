2b9742264a8683e392f556ed7682e546e355af44
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-07-15 13:17:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var k = 0;
+let k = true;
+
 $(document).ready(function(){
 
     $('.clicks').on('click', function(){
-        if(k%2 == 0) $('#box').addClass('show');
-        else $('#box').removeClass('show');
-        k++;
+        if(k){
+            $('#box').addClass('show');
+            k = false;
+        }
+        else{
+            $('#box').removeClass('show');
+            k = true;
+        }
     });
 
     $('.submission').on('click', function(){
         $('#box').removeClass('show');
+        k = true;
+    });
+
+    $('.close_popup').on('click', function(){
+        $('#box').removeClass('show');
+        k = true;
     });
 
 });
